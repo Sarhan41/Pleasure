b531a1e3bc7f0c197eb2db1e353155fd9b0b85d4
Sarhan41 <sp.webdev2024@gmail.com>
2024-06-14 11:27:44 +0530

    refactor: Add WishList component to Header-store directory

---
+++ app/(store)/_components/Header-store/Desktop/WishList.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Heart } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+interface WishListProps {
+  wishlength: number;
+}
+
+const WishList = ({ wishlength }: WishListProps) => {
+  return (
+    <div className="flex items-center gap-x-4">
+ 
+ 
+  </div>
+  );
+};
+
+export default WishList;
