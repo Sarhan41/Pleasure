025d34be1421c419f4292204d1defcf5c34604f6
Sarhan41 <sp.webdev2024@gmail.com>
2024-06-22 16:40:51 +0530

    refactor: Remove Billboard component from the project

--- app/(store)/_components/BestSellerBillboard/index.tsx
+++ app/(store)/_components/BestSellerBillboard/index.tsx
@@ -1,40 +1,31 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-interface BillboardProps {
+interface BestSellerBillboardProps {
   data: {
     imageUrl: string;
+    name: string;
   };
 }
 
-const Billboard: React.FC<BillboardProps> = ({ data }) => {
-  console.log(data);
+const BestSellerBillboard: React.FC<BestSellerBillboardProps> = ({ data }) => {
   return (
     <div className="p-4 sm:p-6 w-full flex justify-center lg:p-8 mt-8 lg:mt-16 overflow-hidden cursor-pointer">
       <Link href="/collections/new" passHref className="w-full h-full">
         <div
-          style={{
-            backgroundImage: `url(${data.imageUrl})`,
-            backgroundSize: "cover",
-          }}
+          style={{ backgroundImage: `url(${data.imageUrl})`, backgroundSize: "cover" }}
           className="relative w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh] rounded-xl overflow-hidden shadow-xl transform transition-transform duration-500 hover:scale-105 bg-center max-sm:bg-left"
         >
           <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center lg:justify-end items-center lg:items-end text-center lg:text-right text-white space-y-4 lg:space-y-6 p-4 sm:p-6 lg:p-10">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold leading-tight drop-shadow-lg">
-              <span className="bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text text-transparent">
-                MONSOON ARRIVALS
-              </span>
-            </h1>
-            <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-xl leading-relaxed max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg text-gray-300">
-              Discover the perfect combination of{" "}
-              <span className="font-bold uppercase text-pink-400">comfort</span>{" "}
-              and{" "}
-              <span className="font-bold uppercase text-yellow-400">style</span>
-              .
-            </p>
+            <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold leading-tight drop-shadow-lg">
+              {data.name}
+            </h2>
+            <Link href="/collections/new" passHref>
             <Button className="mt-4 px-6 sm:px-8 py-2 sm:py-3 bg-gradient-to-r from-pink-500 to-yellow-500 text-white font-bold text-sm sm:text-base rounded-lg shadow-lg transition-transform transform hover:scale-110 hover:shadow-2xl">
+                
               SHOP NOW
             </Button>
+            </Link>
           </div>
         </div>
       </Link>
@@ -42,4 +33,4 @@ const Billboard: React.FC<BillboardProps> = ({ data }) => {
   );
 };
 
-export default Billboard;
+export default BestSellerBillboard;
