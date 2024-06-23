aefcda00e93187e075ba856ce1781f3735090706
Sarhan41 <sp.webdev2024@gmail.com>
2024-06-23 16:51:15 +0530

    refactor: Update BecomeAffiliate page with new design and content

--- app/(store)/(routes)/become-affiliate/page.tsx
+++ app/(store)/(routes)/become-affiliate/page.tsx
@@ -1,17 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const BecomeAffiliate = () => {
   return (
-    <section className="bg-black text-white min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-primary text-5xl font-bold mb-6">
+    <section className="bg-black min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-primary font-serif text-5xl font-bold mb-6">
         HELLO Affiliate
       </h1>
-      <h2 className="text-secondary text-3xl mb-4">
+      <h2 className="text-yellow-300 font-mono text-3xl mb-4">
         We're launching something amazing!
       </h2>
-      <p className="text-gray-400 text-xl">
+      <p className="text-primary font-serif text-3xl">
         Stay tuned, our affiliate program is coming soon.
       </p>
+      <Link href="/" className="mt-6">
+        <Button variant={"outline"}   className="border-primary hover:bg-primary hover:text-white z-50 w-full">Go back to home</Button>
+      </Link>
     </section>
   );
 };
