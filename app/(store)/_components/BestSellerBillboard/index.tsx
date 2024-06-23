baab832db92490155a1b50b6c106edfb27835bb3
Sarhan41 <sp.webdev2024@gmail.com>
2024-06-23 07:42:19 +0530

    refactor: Optimize BestSellerBillboard animation variant for immediate entrance

--- app/(store)/_components/BestSellerBillboard/index.tsx
+++ app/(store)/_components/BestSellerBillboard/index.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { MotionDiv } from "@/constant/MotionElements";
-import {  slideIn } from "@/constant/motion";
+import { slideIn } from "@/constant/motion";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -18,21 +18,20 @@ const BestSellerBillboard: React.FC<BestSellerBillboardProps> = ({
 }) => {
   return (
     <MotionDiv
-      //@ts-ignore
-      variants={slideIn("left", "spring", 0, 1)}
-      initial={"hidden"}
-      animate={"show"}
+      variants={slideIn("left", "easeOut", index*0.01, 0.5)} // Adjusted animation variant for immediate entrance
+      initial="hidden"
+      animate="show"
       className="sm:w-1/2 lg:w-1/3 p-4 flex flex-col items-center justify-center w-full min-h-[300px] mt-8 lg:mt-16 overflow-hidden rounded-3xl shadow-xl transform transition-transform duration-500 hover:scale-105 hover:shadow-2xl border-4 border-transparent hover:border-pink-500"
     >
-      <div  className="relative w-full h-[300px] sm:h-[400px] lg:h-[500px] overflow-hidden rounded-3xl shadow-lg transform transition-transform duration-500 hover:scale-105">
+      <div className="relative w-full h-[300px] sm:h-[400px] lg:h-[500px] overflow-hidden rounded-3xl shadow-lg transform transition-transform duration-500 hover:scale-105">
         <Link href={`/collections/${data.name.replace(/\s+/g, "-")}`} passHref>
-            <Image
-              src={data.imageUrl}
-              alt={data.name}
-              fill
-              objectFit="cover"
-              className="rounded-3xl shadow-lg transform transition-transform duration-500 hover:scale-105"
-            />
+          <Image
+            src={data.imageUrl}
+            alt={data.name}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-3xl shadow-lg transform transition-transform duration-500 hover:scale-105"
+          />
         </Link>
       </div>
       <div className="text-center mt-6">
