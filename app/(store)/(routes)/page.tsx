3ce764d628e79ec971cf72da04ae32883b4b8203
Sarhan41 <sp.webdev2024@gmail.com>
2024-06-23 06:48:39 +0530

    refactor: Update dependencies and sort categories in homepage

--- app/(store)/(routes)/page.tsx
+++ app/(store)/(routes)/page.tsx
@@ -18,18 +18,32 @@ const HomePage = async () => {
     select: { imageUrl: true, name: true },
   });
 
+  const categoryOrder = [
+    "New",
+    "Panties",
+    "Sport Bra",
+    "Camisole",
+    "Shorts",
+    "Pyjama",
+  ];
+
+  // Sort the categories based on the defined order
+  const sortedBillboardData = fourBillboards.sort((a, b) => {
+    return categoryOrder.indexOf(a.name) - categoryOrder.indexOf(b.name);
+  });
+
   return (
     <Container>
-      {billboard && <Billboard data={billboard} />}
+      {/* {billboard && <Billboard data={billboard} />} */}
       <div className="mt-12 text-4xl font-extrabold text-gray-900 text-center leading-tight tracking-tight">
         <span className="block text-transparent bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text">
           Discover
         </span>
         <span className="block">Our Best Sellers</span>
       </div>
-      <div className="grid grid-cols-1  mx-auto px-4 sm:px-6 lg:px-8 sm:grid-cols-2  gap-8 mt-8 ">
-        {fourBillboards.map((billboard) => (
-          <BestSellerBillboard key={billboard.name} data={billboard} />
+      <div className="mx-auto px-4 sm:px-6 lg:px-8 flex flex-wrap gap-8 mt-8 justify-center">
+        {sortedBillboardData.map((billboard, index) => (
+          <BestSellerBillboard key={billboard.name} data={billboard} index={index} />
         ))}
       </div>
     </Container>
--- app/(store)/_components/BestSellerBillboard/index.tsx
+++ app/(store)/_components/BestSellerBillboard/index.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { MotionDiv } from "@/constant/MotionElements";
+import { fadeIn } from "@/constant/motion";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,33 +9,40 @@ interface BestSellerBillboardProps {
     imageUrl: string;
     name: string;
   };
+  index: number;
 }
 
-const BestSellerBillboard: React.FC<BestSellerBillboardProps> = ({ data }) => {
+const BestSellerBillboard: React.FC<BestSellerBillboardProps> = ({
+  data,
+  index,
+}) => {
   return (
-    <div className="flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 mt-8 lg:mt-16 overflow-hidden">
-      <Link href={`/collections/${data.name}`} passHref className="w-full block">
-        <div className="relative w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh] rounded-xl overflow-hidden shadow-2xl transform transition-transform duration-500 hover:scale-105 bg-center">
+    <MotionDiv
+      // @ts-ignore
+      variants={fadeIn("right", "spring", index * 0.2)}
+      className="sm:w-1/2 lg:w-1/3 p-4 flex flex-col items-center justify-center w-full mt-8 lg:mt-16 overflow-hidden rounded-3xl shadow-xl transform transition-transform duration-500 hover:scale-105 hover:shadow-2xl border-4 border-transparent hover:border-pink-500"
+    >
+      <Link href={`/collections/${data.name.replace(/\s+/g, "-")}`} passHref>
+        <div className="relative w-full h-80 sm:h-96 md:h-[60vh] lg:h-[70vh] rounded-3xl shadow-lg transform transition-transform duration-500 hover:scale-105">
           <Image
             src={data.imageUrl}
-            alt=""
+            alt={data.name}
             layout="fill"
-            objectFit="contain"
-            className="rounded-xl"
+            className="rounded-3xl object-cover"
           />
         </div>
       </Link>
-      <div className="text-center mt-4">
-        <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold leading-tight text-gray-900">
+      <div className="text-center mt-6">
+        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold leading-tight text-transparent bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text">
           {data.name}
         </h2>
-        <Link href="/collections/new" passHref>
-          <Button className="mt-2 px-6 sm:px-8 py-2 sm:py-3 bg-gradient-to-r from-pink-500 to-yellow-500 text-white font-bold text-sm sm:text-base rounded-lg shadow-lg transition-transform transform hover:scale-110 hover:shadow-2xl font-mono">
+        <Link href={`/collections/${data.name.replace(/\s+/g, "-")}`} passHref>
+          <Button className="mt-4 px-8 py-3 bg-gradient-to-r from-pink-500 to-yellow-500 text-white text-lg font-semibold rounded-xl shadow-lg transform transition-transform hover:scale-110 hover:shadow-2xl">
             SHOP NOW
           </Button>
         </Link>
       </div>
-    </div>
+    </MotionDiv>
   );
 };
 
