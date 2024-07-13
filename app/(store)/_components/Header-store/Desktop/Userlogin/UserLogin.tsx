626067dba74f743d76b81708b302851967a55489
Sarhan41 <sp.webdev2024@gmail.com>
2024-07-13 16:24:35 +0530

    refactor: Update UserLogin component in DesktopHeader
    The UserLogin component in the DesktopHeader has been updated to improve its import path. This change ensures that the correct UserLogin component is imported, enhancing the accuracy of the code.

--- app/(store)/_components/Header-store/Desktop/Userlogin/UserLogin.tsx
+++ app/(store)/_components/Header-store/Desktop/Userlogin/UserLogin.tsx
@@ -86,7 +86,7 @@ const UserLogin = ({ userId }: UserLoginProps) => {
                     <>
                       <div className="flex flex-col justify-center gap-3">
                         <div className="z-50"></div>
-                        <LoginButton asChild mode="modal">
+                        <LoginButton asChild>
                           <Button
                             variant="outline"
                             className="border-primary hover:bg-primary z-50 hover:text-white"
@@ -96,7 +96,7 @@ const UserLogin = ({ userId }: UserLoginProps) => {
                           </Button>
                         </LoginButton>
                         <div className="z-50"></div>
-                        <SignUpButton asChild mode="modal">
+                        <SignUpButton asChild >
                           <Button
                             size="lg"
                             variant="outline"
