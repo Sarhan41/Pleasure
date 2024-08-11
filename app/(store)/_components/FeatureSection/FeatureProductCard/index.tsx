f04f38679a4bd474a20b18e40f91f87699e9bce0
Sarhan41 <sp.webdev2024@gmail.com>
2024-08-11 22:43:28 +0530

    refactor: Update Gallery-Page component for improved image display

--- app/(store)/_components/FeatureSection/FeatureProductCard/index.tsx
+++ app/(store)/_components/FeatureSection/FeatureProductCard/index.tsx
@@ -30,7 +30,7 @@ const FeaturedProductCard: React.FC<FeaturedProductCardProps> = ({
   const handleClick = () => {
     const productName = data?.name.replace(/\s+/g, "-");
     if (!isModalOpen) {
-      router.push(`/product/${productName}`);
+      window.open(`/product/${productName}`, "_blank");
     }
   };
 
--- app/(store)/_components/Header-store/Mobile/Sidebar/SecondRowCategory.tsx
+++ app/(store)/_components/Header-store/Mobile/Sidebar/SecondRowCategory.tsx
@@ -29,27 +29,35 @@ const SecondRowCategory = ({
       <h2 className="text-lg font-semibold text-gray-800 mb-4">Categories</h2>
       {categories.map((category) => (
         <div key={category.id} className="mb-2">
-          <button
-            className="flex items-center justify-between w-full px-3 py-2 text-base font-medium text-gray-700 rounded-md hover:bg-primary-100 transition-colors duration-200"
-            onClick={() => handleCategoryClick(category.name)}
-          >
-            <Link area-label="Link" href={`/collections/${category.name.replace(/\s+/g, "-")}`}>
+          <div className=" flex items-center justify-between w-full px-3 py-2 text-base font-medium text-gray-700 rounded-md hover:bg-primary-100 transition-colors duration-200">
+            <Link
+              area-label="Link"
+              href={`/collections/${category.name.replace(/\s+/g, "-")}`}
+              onClick={toggleSidebar}
+            >
               <div className="flex items-center gap-2 text-primary-600">
                 {category.name}
               </div>
             </Link>
-            {expandedCategory === category.name ? (
-              <ChevronDownIcon size={20} className="text-primary-600" />
-            ) : (
-              <ChevronRightIcon size={20} className="text-primary-600" />
-            )}
-          </button>
+            <button
+              className=""
+              onClick={() => handleCategoryClick(category.name)}
+            >
+              {expandedCategory === category.name ? (
+                <ChevronDownIcon size={20} className="text-primary-600" />
+              ) : (
+                <ChevronRightIcon size={20} className="text-primary-600" />
+              )}
+            </button>
+          </div>
+
           {expandedCategory === category.name && (
             <div className="pl-8 mt-2 max-h-60 overflow-y-auto">
               {category.products.slice(0, 8).map((product, index) => (
                 <React.Fragment key={product.id}>
                   <div className="flex">
-                    <Link area-label="Link"
+                    <Link
+                      area-label="Link"
                       href={`/product/${product.name.replace(/\s+/g, "-")}`}
                       className="block px-3 py-1 text-base font-medium text-gray-600 rounded-md hover:bg-primary-100 transition-colors duration-200"
                       onClick={toggleSidebar}
