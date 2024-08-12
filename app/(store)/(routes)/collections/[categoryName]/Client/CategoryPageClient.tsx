98fd14995a6d7336efdd2dce4e28f8430e527d88
Sarhan41 <sp.webdev2024@gmail.com>
2024-08-12 11:12:46 +0530

    refactor: Remove MobileFilter component
    The MobileFilter component in the app/(store)/(routes)/collections/[categoryName]/components/MobileFilter.tsx file has been removed as it is no longer used in the codebase. This cleanup improves code maintainability and reduces unnecessary complexity.

--- app/(store)/(routes)/collections/[categoryName]/Client/CategoryPageClient.tsx
+++ app/(store)/(routes)/collections/[categoryName]/Client/CategoryPageClient.tsx
@@ -2,8 +2,10 @@
 
 import React, { useState, useMemo } from "react";
 import ProductCard from "@/app/(store)/_components/ProductCard/ProductCard";
-import { Product, Size, Color } from "@/types";
-import MobileFilters from "../components/MobileFilter";
+import { Product } from "@/types";
+import { Dialog, Transition } from "@headlessui/react"; // Importing Dialog and Transition components
+import { Fragment } from "react";
+import { PlusIcon } from "lucide-react";
 
 interface CategoryPageClientProps {
   products: Product[];
@@ -20,18 +22,25 @@ const CategoryPageClient: React.FC<CategoryPageClientProps> = ({
   const [selectedSizes, setSelectedSizes] = useState<string[]>([]);
   const [priceRange, setPriceRange] = useState<[number, number]>([0, Infinity]);
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+  const [isFilterOpen, setIsFilterOpen] = useState(false); // State to control dialog visibility
 
-  const colorOptions = useMemo(
-    () =>
-      Array.from(new Set(products.flatMap((p) => p.colors.map((c) => c.name)))),
-    [products]
-  );
+  // Extract unique color names
+  const colorOptions = useMemo(() => {
+    const allColors = products.flatMap((p) => p.colors);
+    const uniqueColors = Array.from(new Set(allColors.map((c) => c.name))).map(
+      (name) => allColors.find((c) => c.name === name)!
+    );
+    return uniqueColors;
+  }, [products]);
 
-  const sizeOptions = useMemo(
-    () =>
-      Array.from(new Set(products.flatMap((p) => p.sizes.map((s) => s.name)))),
-    [products]
-  );
+  // Extract unique size names
+  const sizeOptions = useMemo(() => {
+    const allSizes = products.flatMap((p) => p.sizes);
+    const uniqueSizes = Array.from(new Set(allSizes.map((s) => s.name))).map(
+      (name) => allSizes.find((s) => s.name === name)!
+    );
+    return uniqueSizes;
+  }, [products]);
 
   const minPrice = useMemo(
     () =>
@@ -96,7 +105,16 @@ const CategoryPageClient: React.FC<CategoryPageClientProps> = ({
   return (
     <div className="container mx-auto p-4">
       <div className="flex flex-col lg:flex-row gap-6">
-        {/* Filters Column */}
+        {/* Filters Button for Mobile */}
+        <button
+          className="lg:hidden bg-gradient-to-r from-purple-500 to-indigo-500 text-white text-lg font-semibold px-4 py-3 rounded-full flex items-center justify-center w-fit mb-4 shadow-lg hover:from-purple-600 hover:to-indigo-600 transition-transform transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-indigo-300"
+          onClick={() => setIsFilterOpen(true)}
+        >
+          Filters
+          <PlusIcon className="w-6 h-6 ml-2 text-white" />
+        </button>
+
+        {/* Filters Column for Desktop */}
         <div className="w-full lg:w-1/4 bg-gray-50 p-6 rounded-lg h-fit shadow-md hidden lg:block">
           <h2 className="text-3xl font-extrabold mb-8 text-gray-800">
             Filters
@@ -109,21 +127,21 @@ const CategoryPageClient: React.FC<CategoryPageClientProps> = ({
             <div className="flex flex-wrap gap-3">
               {colorOptions.map((color) => (
                 <button
-                  key={color}
+                  key={color.name}
                   className={`rounded-lg text-sm px-3 py-2 font-medium border transition-all duration-200 ${
-                    selectedColors.includes(color)
+                    selectedColors.includes(color.name)
                       ? "bg-primary text-white border-primary"
                       : "bg-white text-gray-700 border-gray-300 hover:bg-gray-100"
                   }`}
                   onClick={() =>
                     setSelectedColors((prev) =>
-                      prev.includes(color)
-                        ? prev.filter((c) => c !== color)
-                        : [...prev, color]
+                      prev.includes(color.name)
+                        ? prev.filter((c) => c !== color.name)
+                        : [...prev, color.name]
                     )
                   }
                 >
-                  {color}
+                  {color.name}
                 </button>
               ))}
             </div>
@@ -136,21 +154,21 @@ const CategoryPageClient: React.FC<CategoryPageClientProps> = ({
             <div className="flex flex-wrap gap-3">
               {sizeOptions.map((size) => (
                 <button
-                  key={size}
+                  key={size.name}
                   className={`rounded-lg text-sm px-3 py-2 font-medium border transition-all duration-200 ${
-                    selectedSizes.includes(size)
+                    selectedSizes.includes(size.name)
                       ? "bg-primary text-white border-primary"
                       : "bg-white text-gray-700 border-gray-300 hover:bg-gray-100"
                   }`}
                   onClick={() =>
                     setSelectedSizes((prev) =>
-                      prev.includes(size)
-                        ? prev.filter((s) => s !== size)
-                        : [...prev, size]
+                      prev.includes(size.name)
+                        ? prev.filter((s) => s !== size.name)
+                        : [...prev, size.name]
                     )
                   }
                 >
-                  {size}
+                  {size.name}
                 </button>
               ))}
             </div>
@@ -185,6 +203,155 @@ const CategoryPageClient: React.FC<CategoryPageClientProps> = ({
           </div>
         </div>
 
+        {/* Filter Dialog for Mobile */}
+        <Transition appear show={isFilterOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="fixed inset-0 z-50 overflow-y-auto"
+            onClose={() => setIsFilterOpen(false)}
+          >
+            <div className="min-h-screen px-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+              </Transition.Child>
+
+              {/* Trick to center the modal content */}
+              <span
+                className="inline-block h-screen align-middle"
+                aria-hidden="true"
+              >
+                &#8203;
+              </span>
+
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                  <h2 className="text-3xl font-extrabold mb-8 text-gray-800">
+                    Filters
+                  </h2>
+
+                  {/* Filter by Color */}
+                  <div className="mb-10">
+                    <h3 className="text-xl font-semibold text-gray-700">
+                      Colors
+                    </h3>
+                    <hr className="my-4 border-gray-300" />
+                    <div className="flex flex-wrap gap-3">
+                      {colorOptions.map((color) => (
+                        <button
+                          key={color.name}
+                          className={`rounded-lg text-sm px-3 py-2 font-medium border transition-all duration-200 ${
+                            selectedColors.includes(color.name)
+                              ? "bg-primary text-white border-primary"
+                              : "bg-white text-gray-700 border-gray-300 hover:bg-gray-100"
+                          }`}
+                          onClick={() =>
+                            setSelectedColors((prev) =>
+                              prev.includes(color.name)
+                                ? prev.filter((c) => c !== color.name)
+                                : [...prev, color.name]
+                            )
+                          }
+                        >
+                          {color.name}
+                        </button>
+                      ))}
+                    </div>
+                  </div>
+
+                  {/* Filter by Size */}
+                  <div className="mb-10">
+                    <h3 className="text-xl font-semibold text-gray-700">
+                      Sizes
+                    </h3>
+                    <hr className="my-4 border-gray-300" />
+                    <div className="flex flex-wrap gap-3">
+                      {sizeOptions.map((size) => (
+                        <button
+                          key={size.name}
+                          className={`rounded-lg text-sm px-3 py-2 font-medium border transition-all duration-200 ${
+                            selectedSizes.includes(size.name)
+                              ? "bg-primary text-white border-primary"
+                              : "bg-white text-gray-700 border-gray-300 hover:bg-gray-100"
+                          }`}
+                          onClick={() =>
+                            setSelectedSizes((prev) =>
+                              prev.includes(size.name)
+                                ? prev.filter((s) => s !== size.name)
+                                : [...prev, size.name]
+                            )
+                          }
+                        >
+                          {size.name}
+                        </button>
+                      ))}
+                    </div>
+                  </div>
+
+                  {/* Filter by Price */}
+                  <div className="mb-10">
+                    <h3 className="text-xl font-semibold text-gray-700">
+                      Prices
+                    </h3>
+                    <hr className="my-4 border-gray-300" />
+                    <input
+                      type="range"
+                      min={minPrice}
+                      max={maxPrice}
+                      value={priceRange[1]}
+                      onChange={(e) =>
+                        setPriceRange([
+                          priceRange[0],
+                          parseFloat(e.target.value),
+                        ])
+                      }
+                      className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer focus:outline-none focus:ring-2 focus:ring-primary"
+                      style={{
+                        background: `linear-gradient(to right, #4f46e5 0%, #4f46e5 ${
+                          ((priceRange[1] - minPrice) / (maxPrice - minPrice)) *
+                          100
+                        }%, #d1d5db ${
+                          ((priceRange[1] - minPrice) / (maxPrice - minPrice)) *
+                          100
+                        }%, #d1d5db 100%)`,
+                      }}
+                    />
+                    <div className="mt-4 text-sm text-gray-600">
+                      <span>{`Min: ${priceRange[0].toFixed(
+                        2
+                      )} - Max: ${priceRange[1].toFixed(2)}`}</span>
+                    </div>
+                  </div>
+
+                  <div className="flex justify-end">
+                    <button
+                      className="bg-primary text-white px-4 py-2 rounded-md"
+                      onClick={() => setIsFilterOpen(false)}
+                    >
+                      Apply Filters
+                    </button>
+                  </div>
+                </div>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition>
+
         {/* Products Column */}
         <div className="w-full lg:w-3/4">
           <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-4">
@@ -216,27 +383,10 @@ const CategoryPageClient: React.FC<CategoryPageClientProps> = ({
             </div>
           </div>
 
-          <MobileFilters
-            sizes={sizeOptions as Size[]}
-            colors={colorOptions as Color[]}
-            selectedColors={selectedColors}
-            selectedSizes={selectedSizes}
-            priceRange={priceRange}
-            minPrice={minPrice}
-            maxPrice={maxPrice}
-            setSelectedColors={setSelectedColors}
-            setSelectedSizes={setSelectedSizes}
-            setPriceRange={setPriceRange}
-          />
-
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {sortedProducts.length > 0 ? (
-              sortedProducts.map((product) => (
-                <ProductCard key={product.id} data={product} userId={userId} />
-              ))
-            ) : (
-              <p className="text-lg text-gray-500">No results found.</p>
-            )}
+            {sortedProducts.map((product) => (
+              <ProductCard key={product.id} data={product} userId={userId} />
+            ))}
           </div>
         </div>
       </div>
