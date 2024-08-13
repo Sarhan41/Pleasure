f8cb51e9d0ba0212c253b9a60c14ec403cfd41fc
Sarhan41 <sp.webdev2024@gmail.com>
2024-08-13 09:49:01 +0530

    Update the SummaryCheckout and SummaryCart components to display an updated shipping message. The message now includes an asterisk (*) to indicate that free shipping is available on orders over ₹500. This change improves clarity and ensures consistency between the two components.

--- app/(store)/(routes)/cart/checkout/components/SummaryCheckout.tsx
+++ app/(store)/(routes)/cart/checkout/components/SummaryCheckout.tsx
@@ -64,7 +64,7 @@ const SummaryCheckout: React.FC<SummaryProps> = ({
         </div>
         {orderTotal < 500 && (
           <div className="text-sm text-red-500 mt-4">
-            <p>Free shipping on orders over ₹500</p>
+            <p>Free shipping on orders over ₹500*</p>
           </div>
         )}
       </div>
