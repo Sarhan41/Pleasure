ec10ab569395610e4bea2cc171901798431f976d
Sarhan41 <sp.webdev2024@gmail.com>
2024-08-13 09:36:02 +0530

    refactor: Update SummaryCheckout component for improved order total calculation

--- app/(store)/(routes)/cart/checkout/components/SummaryCheckout.tsx
+++ app/(store)/(routes)/cart/checkout/components/SummaryCheckout.tsx
@@ -29,31 +29,30 @@ const SummaryCheckout: React.FC<SummaryProps> = ({
 
   return (
     <div className="max-sm:mt-8 rounded-lg max-md:w-fit md:min-w-full bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8 shadow-md">
-      <h2 className="text-lg font-medium text-gray-900 sm:text-xl md:text-2xl lg:text-3xl">
+      <h2 className="text-lg font-semibold text-gray-900 sm:text-xl md:text-2xl lg:text-3xl">
         Order Summary
       </h2>
-      <div className="mt-6 space-y-4">
+      <div className="mt-6 space-y-6">
         <div className="flex items-center w-full justify-between text-sm sm:text-base md:text-lg lg:text-base">
-          <div className="font-medium text-gray-900">Sub Total</div>
+          <span className="font-semibold text-gray-900">Sub Total</span>
           <Currency value={orderTotal} />
         </div>
         {discount > 0 && (
           <div className="flex items-center w-full justify-between border-t border-gray-200 pt-4 text-sm sm:text-base md:text-lg lg:text-base">
-            <div className="font-medium text-gray-900">Discount</div>
+            <span className="font-semibold text-gray-900">Discount</span>
             <Currency value={discount} />
           </div>
         )}
-
         <div className="flex items-center w-full justify-between border-t border-gray-200 pt-4 text-sm sm:text-base md:text-lg lg:text-base">
-          <div className="font-medium text-gray-900">Tax</div>
+          <span className="font-semibold text-gray-900">Tax</span>
           <Currency value={Math.round(orderTotal * 0.05)} />
         </div>
         <div className="flex items-center justify-between border-t border-gray-200 pt-4 text-sm sm:text-base md:text-lg lg:text-base">
-          <div className="font-medium text-gray-900">Shipping</div>
+          <span className="font-semibold text-gray-900">Shipping</span>
           <Currency value={orderTotal > 500 ? 0 : 50} />
         </div>
         <div className="flex items-center justify-between border-t border-gray-200 pt-4 text-sm sm:text-base md:text-lg lg:text-base">
-          <div className="font-medium text-gray-900">You Pay</div>
+          <span className="font-bold text-gray-900">You Pay</span>
           <Currency
             value={
               orderTotal -
