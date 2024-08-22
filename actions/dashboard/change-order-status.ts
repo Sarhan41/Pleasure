74ba6455871856f0f5f5f9765212a30c8488aca8
Sarhan41 <sp.webdev2024@gmail.com>
2024-08-22 07:12:21 +0530

    refactor: Update CellAction component to toggle order status in DataTable

---
+++ actions/dashboard/change-order-status.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { db } from "@/lib/db";
+import toast from "react-hot-toast";
+
+interface OrderStatusProps {
+  id: string;
+  status: string;
+}
+
+export const changeOrderStatus = async ({ id, status }: OrderStatusProps) => {
+  try {
+    const updatedOrder = await db.order.update({
+      where: {
+        id: id,
+      },
+      data: {
+        status: status === "Completed" ? "Pending" : "Completed",
+      },
+    });
+
+    // Return the updated order object
+    return updatedOrder;
+  } catch (error) {
+    // Handle errors
+    console.log("[Order_PATCH]", error);
+    throw new Error("Error changing order status");
+  }
+};
