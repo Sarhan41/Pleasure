a7fc7bb23be183ff0e70bbe93e5f2a4aff441a7f
Sarhan41 <sp.webdev2024@gmail.com>
2024-08-26 08:03:13 +0530

    refactor: Add top margin to RelatedBlogList component in blog/[blogName] route

--- app/(store)/(routes)/blog/[blogName]/components/RelatedBlogList.tsx
+++ app/(store)/(routes)/blog/[blogName]/components/RelatedBlogList.tsx
@@ -15,7 +15,7 @@ const RelatedBlogList: React.FC<RelatedBlogListProps> = ({ blogs }) => {
      <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-extrabold text-start leading-tight drop-shadow-xl bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent border-b-4 border-b-black w-fit pb-3">
         Related <span>Blogs</span>
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 mt-8 lg:grid-cols-3 gap-6">
         {blogs.map((blog) => (
           <BlogCard key={blog.id} data={blog} />
         ))}
