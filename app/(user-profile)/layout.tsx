f126e53e0f16c160e8f43229eafebec1875cdb6c
Sarhan41 <sp.webdev2024@gmail.com>
2024-09-01 20:50:42 +0530

    refactor: Update Footer component with correct Instagram route and add Footer to UserProfile layout

--- app/(user-profile)/layout.tsx
+++ app/(user-profile)/layout.tsx
@@ -2,6 +2,7 @@ import { currentUser } from "@/lib/auth";
 import CheckoutHeader from "../(store)/_components/Header-store/Checkout";
 import { Navbar } from "./_components/navbar";
 import { CardWrapper } from "@/components/Auth/AuthUi/CardWrapper";
+import Footer from "../(store)/_components/Footer/Footer";
 
 interface ProtectedLayoutProps {
   children: React.ReactNode;
@@ -14,7 +15,7 @@ const ProtectedLayout = async ({ children }: ProtectedLayoutProps) => {
   if (!user) {
     return (
       <>
-        <CheckoutHeader  />
+        <CheckoutHeader />
         <div className=" flex  h-screen w-screen justify-center items-center ">
           <CardWrapper
             headerLabel="You are Not Logged IN"
@@ -29,15 +30,18 @@ const ProtectedLayout = async ({ children }: ProtectedLayoutProps) => {
   }
 
   return (
-    <div className="h-fit w-screen flex flex-col items-center justify-center mb-20">
-      <CheckoutHeader  />
-      <div className="w-full px-4 sm:px-10 md:px-16 mt-4 lg:px-24 xl:px-40">
-        <Navbar />
+    <>
+      <div className="h-fit w-screen flex flex-col items-center justify-center mb-20">
+        <CheckoutHeader />
+        <div className="w-full px-4 sm:px-10 md:px-16 mt-4 lg:px-24 xl:px-40">
+          <Navbar />
+        </div>
+        <div className="flex-grow w-full mt-4 flex justify-center items-center">
+          {children}
+        </div>
       </div>
-      <div className="flex-grow w-full mt-4 flex justify-center items-center">
-        {children}
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
