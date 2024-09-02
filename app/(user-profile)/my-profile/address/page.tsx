e56da08a3e094b33a1624d88329130d83f393233
Sarhan41 <sp.webdev2024@gmail.com>
2024-09-02 10:17:54 +0530

    Cache Updata

--- app/(user-profile)/my-profile/address/page.tsx
+++ app/(user-profile)/my-profile/address/page.tsx
@@ -1,16 +1,25 @@
 import { currentUser } from "@/lib/auth";
-import { UserInfo } from "@/components/Auth/AuthUi/UserInfo";
 import { db } from "@/lib/db";
 import { AddressInfo } from "@/components/Auth/AuthUi/AddressInfo";
+import { unstable_cache as cache } from "next/cache";
+
+// Define cache function
+const getAddresses = cache(async (userId: string | undefined) => {
+  return await db.address.findMany({
+    where: {
+      userId,
+    },
+  });
+});
 
 const MyProfileAddressPage = async () => {
   const user = await currentUser();
+  if (!user) {
+    // Handle case where user is not authenticated
+    return <div>User not authenticated</div>;
+  }
 
-  const addresses = await db.address.findMany({
-    where: {
-      userId: user?.id,
-    },
-  });
+  const addresses = await getAddresses(user.id);
 
   return <AddressInfo label="My Addresses" addresses={addresses} />;
 };
