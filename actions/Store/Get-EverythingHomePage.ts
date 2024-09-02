8fd0ab841ab1588e5729a3bcd4c0aadf0803d474
Sarhan41 <sp.webdev2024@gmail.com>
2024-09-02 07:22:29 +0530

    refactor: Update Get-EverythingHomePage actions to use cache for improved performance

--- actions/Store/Get-EverythingHomePage.ts
+++ actions/Store/Get-EverythingHomePage.ts
@@ -2,38 +2,33 @@
 import { db } from "@/lib/db";
 import { unstable_cache as cache } from "next/cache";
 
-export const getBillboards = cache(async () => {
-  const billboards = await db.billboard.findMany({
-    select: {
-      imageUrl: true,
-      name: true,
-      title: true,
-      subtitle: true,
-      link: true,
-    },
-  });
-  return billboards;
-});
+export const getHomePageData = cache(async () => {
+  const [billboards, categories, featuredProducts] = await Promise.all([
+    db.billboard.findMany({
+      select: {
+        imageUrl: true,
+        name: true,
+        title: true,
+        subtitle: true,
+        link: true,
+      },
+    }),
+    db.category.findMany({
+      select: { imageUrl: true, name: true },
+    }),
+    db.product.findMany({
+      where: {
+        isFeatured: true,
+      },
+      include: {
+        category: true,
+        images: true,
+        sizes: true,
+        colors: true,
+        colorNames: true,
+      },
+    }),
+  ]);
 
-export const getCategories = cache(async () => {
-  const categories = await db.category.findMany({
-    select: { imageUrl: true, name: true },
-  });
-  return categories;
-});
-
-export const getFeaturedProducts = cache(async () => {
-  const featuredProducts = await db.product.findMany({
-    where: {
-      isFeatured: true,
-    },
-    include: {
-      category: true,
-      images: true,
-      sizes: true,
-      colors: true,
-      colorNames: true,
-    },
-  });
-  return featuredProducts;
+  return { billboards, categories, featuredProducts };
 });
