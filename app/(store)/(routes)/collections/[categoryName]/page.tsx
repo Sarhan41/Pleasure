d3769e54a1329bcb8a5bd94b36a463641ae9d63b
Sarhan41 <sp.webdev2024@gmail.com>
2024-09-02 10:17:36 +0530

    Cache Updata

--- app/(store)/(routes)/collections/[categoryName]/page.tsx
+++ app/(store)/(routes)/collections/[categoryName]/page.tsx
@@ -1,6 +1,37 @@
 import { db } from "@/lib/db";
 import CategoryPageClient from "./Client/CategoryPageClient";
 import { currentUser } from "@/lib/auth";
+import { unstable_cache as cache } from "next/cache";
+
+// Define cache functions
+const getCategory = cache(async (categoryName: string) => {
+  return await db.category.findFirst({
+    where: {
+      name: categoryName,
+    },
+    select: {
+      id: true,
+      imageUrl: true,
+      title: true,
+    },
+  });
+});
+
+const getProducts = cache(async (categoryId: string) => {
+  return await db.product.findMany({
+    where: {
+      categoryId: categoryId,
+      isArchived: false,
+    },
+    include: {
+      sizes: true,
+      colors: true,
+      category: true,
+      images: true,
+      colorNames: true,
+    },
+  });
+});
 
 interface CategoryPageProps {
   params: {
@@ -16,30 +47,13 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({ params }) => {
   const user = await currentUser();
   const userId = user?.id;
 
-  const category = await db.category.findFirst({
-    where: {
-      name: categoryName,
-    },
-    select: {
-      id: true,
-      imageUrl: true,
-      title: true,
-    },
-  });
+  const category = await getCategory(categoryName);
+  if (!category) {
+    // Handle the case where the category is not found
+    return <div>Category not found</div>;
+  }
 
-  const products = await db.product.findMany({
-    where: {
-      categoryId: category?.id,
-      isArchived: false,
-    },
-    include: {
-      sizes: true,
-      colors: true,
-      category: true,
-      images: true,
-      colorNames: true,
-    },
-  });
+  const products = await getProducts(category.id);
 
   return (
     <CategoryPageClient
